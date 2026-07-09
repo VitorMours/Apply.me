51e743d4023ed716364c4de23744e1422429073c
VitorMoura <jvrezendemoura@gmail.com>
2026-07-09 10:02:37 -0300

    Creating the tests markers

--- backend/app/schemas/auth_schemas.py
+++ backend/app/schemas/auth_schemas.py
@@ -2,8 +2,6 @@ from pydantic import BaseModel, Field
 from typing import Optional 
 from datetime import datetime 
 
-
-
 class Credentials(BaseModel):
     email: str 
     password: str 
@@ -13,7 +11,6 @@ class LoginCredentials(Credentials):
 
 class SigninCredentials(Credentials):
     name: str 
-    
     
 class ReceiveToken(BaseModel):
     access_token: str
--- backend/app/test/conftest.py
+++ backend/app/test/conftest.py
@@ -4,6 +4,32 @@ from mongomock_motor import AsyncMongoMockClient
 from app.models.user_model import User
 from app.models.application_model import Application  # Importe o modelo aqui
 
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "models: testes unitários rápidos"
+    )
+    config.addinivalue_line(
+        "markers", "services: testes de integração"
+    )
+    config.addinivalue_line(
+        "markers", "schemas: testes que demoram mais"
+    )
+    config.addinivalue_line(
+        "markers", "api: testes críticos de smoke test"
+    )
+
+def pytest_collection_modifyitems(config, items):
+    for item in items:
+        path = str(item.fspath)
+        if "/unit/" in path:
+            item.add_marker("unit")
+        elif "/integration/" in path:
+            item.add_marker("integration")
+            
+            
+# TODO: Precisa organizar os markers acima, ainda.
+
+
 @pytest_asyncio.fixture(scope="function")
 async def mock_db():
     client = AsyncMongoMockClient()

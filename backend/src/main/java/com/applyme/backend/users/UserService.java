bab84314bbb62e1a88b34bd833178ae9a713b5b7
VitorMoura <jvrezendemoura@gmail.com>
2026-07-28 22:08:34 -0300

    Creating the auth files and tests

--- backend/src/main/java/com/applyme/backend/users/UserService.java
+++ backend/src/main/java/com/applyme/backend/users/UserService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import com.applyme.backend.users.internal.UserProfile;
 import com.applyme.backend.users.internal.UserProfileRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -29,6 +31,23 @@ public class UserService {
         return Optional.empty();
     }
     
-    
-    // public void getProfileById(){}
+    public Optional<String> getProfileById(String id){
+        Optional<UserProfile> profileSearched = repository.findById(id);
+        if (profileSearched.isPresent()) {
+            return Optional.of(profileSearched.get().getEmail());
+        }
+        return Optional.empty();
+    }
+
+    public boolean deleteProfile(String id) {
+        Optional<UserProfile> profileSearched = repository.findById(id);
+        if (profileSearched.isEmpty()) {
+            throw new EntityNotFoundException("Não foi encontrada uma entidade com o id: " + id);
+        }
+
+        UserProfile profile = profileSearched.get();
+        profile.setActive(false);
+        repository.save(profile);
+        return true;
+    }
 }
